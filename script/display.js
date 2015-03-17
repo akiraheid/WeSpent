9284be7810b402dd64e931a7888927764b5e9b8f
Michael Heid <mheid2011@gmail.com>
2015-03-16 20:56:13 -0400

    Finalized logic of adding and removing participant rows.

--- script/display.js
+++ script/display.js
@@ -1,27 +1,22 @@
 // This script file is for handling the display aspect of WeSpent
 
+// Toggle the visibility of a div.
+//
+// checkbox       The checkbox that was checked.
+// showHideDivID  The id of the div to toggle the visibility of.
 function toggleCheckBox(checkbox, showHideDivID)
 {
   if(!checkbox.checked)
   {
-    toggleDiv(showHideDivID, "block");
+    document.getElementById(showHideDivID).style.display = "block";
   }
   else
   {
-    toggleDiv(showHideDivID, "none");
+    document.getElementById(showHideDivID).style.display = "none";
   }
 }
 
-// Toggle visibility of the specified div.
-//
-// showHideDivID  The ID of the div to show/hide.
-function toggleDiv(showHideDivID, value)
-{
-  var element = document.getElementById(showHideDivID);
-  element.style.display = value;
-}
-
-// Make
+// Uncheck all of the checkboxes.
 function uncheckCheckBoxes()
 {
   var elements = document.getElementsByClassName("checkbox");
@@ -31,4 +26,49 @@ function uncheckCheckBoxes()
   }
 }
 
-window.onload = uncheckCheckBoxes;
+// Add a participant row to the participant table.
+function addParticipantRow()
+{
+  var ni = document.getElementById('participant-rows');
+  var numi = document.getElementById('theValue');
+  var newRow = document.createElement('div');
+  var num = ++numi.value;
+  newRow.setAttribute('class', "participant-row");
+  newRow.setAttribute('id', "row" + num);
+  newRow.innerHTML = "<input type=text class=\"input-field\"></input> \
+                  <input type=password class=\"salary-field\"></input> \
+              <div class=\"remove-wrapper\" onclick=\"removeParticipantRow("+num+")\"> \
+                <div class=\"remove\">-</div> \
+              </div>";
+  ni.appendChild(newRow);
+  
+  // Since we've added a row, we can allow other rows to be deleted with the
+  // remove button.
+  if(document.getElementsByClassName("participant-row").length == 2)
+  {
+    document.getElementsByClassName("remove-no")[0].setAttribute('class', "remove");
+  }
+}
+
+// Remove the selected element unless it is the last row of the participant
+// table.
+//
+// rowNum  The row ID number to delete.
+function removeParticipantRow(rowNum)
+{
+  var rows = document.getElementsByClassName("participant-row");
+  if(rows.length != 1)
+  {
+    document.getElementById('participant-rows').removeChild(
+        document.getElementById("row"+rowNum) ); 
+    if(rows.length == 1)
+      document.getElementsByClassName("remove")[0].setAttribute('class', "remove-no");
+  }
+}
+
+// Function to initialize the page.
+function init(){
+  uncheckCheckBoxes();
+}
+
+window.onload = init;
